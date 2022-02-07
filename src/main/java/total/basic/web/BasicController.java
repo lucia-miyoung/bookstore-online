773901b29d0daff7dd994df3ad3161d86c906ece
JUNGMIYOUNG <myjung93a@gmail.com>
2022-02-08 08:44:00 +0900

    add service-center q&a 로직과 페이지 추가 and 페이징 구현

--- src/main/java/total/basic/web/BasicController.java
+++ src/main/java/total/basic/web/BasicController.java
@@ -13,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import common.CommonController;
+import common.PageUtil;
 import total.basic.service.BasicService;
+
 
 @Controller
 @RequestMapping("/common")
 public class BasicController extends CommonController{
 
-	
+	@Resource(name = "pageUtil")
+	private PageUtil pageUtil;
+
 	@Resource(name="BasicService")
 	private BasicService service;	
 	
@@ -56,7 +60,37 @@ public class BasicController extends CommonController{
 			System.out.println(result);
 		return result;		
 	}
-	
-	
+	@RequestMapping(value="/showqstlist")
+	public String showqstlist(HttpServletRequest req, HttpServletResponse res, @RequestParam HashMap paramMap,
+			ModelMap model) throws Exception {
+			System.out.println("showqstlist {}:  " + paramMap);
+			int totalCnt = service.qstTotalListCnt(paramMap);
+
+			if(paramMap.get("currPage")==null) {
+				paramMap.put("currPage" , '1');
+			}		
+			paramMap.put("listCnt", totalCnt);
+			pageUtil.setPage(paramMap); 
+			pageUtil.setListCnt(totalCnt);
+			System.out.println("페이지 시작: " + pageUtil.getStartPage());
+			System.out.println("페이지 끝: " + pageUtil.getEndPage());
+			System.out.println("페이지 수: " + pageUtil.getPageCnt());
+			System.out.println("총 개수: " + totalCnt);
+			HashMap rsMap = new HashMap(paramMap);
+			rsMap.put("pageCnt", pageUtil.getPageCnt());
+			rsMap.put("startPage", pageUtil.getStartPage());
+			rsMap.put("endPage", pageUtil.getEndPage());
+			rsMap.put("preBtn", pageUtil.isPrev());
+			rsMap.put("nextBtn", pageUtil.isNext());
+			rsMap.put("rowNum", pageUtil.getRowNum());
+			model.addAttribute("paramMap", paramMap);
+			model.addAttribute("qstallList", service.qstTotalList(paramMap));
+			model.addAttribute("currPage", paramMap.get("currPage"));
+			model.addAttribute("paging",rsMap);
+			System.out.println("paramMap:" + paramMap);
+			System.out.println(rsMap);
+			return "/main/question";
+	}
+
 	
 }
